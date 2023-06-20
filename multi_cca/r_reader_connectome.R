8eb6a5a4063e3f482b637a5fcb747339f56f3cc3
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2023-06-20 12:02:27 -0400

    Add files via upload

--- multi_cca/r_reader_connectome.R
+++ multi_cca/r_reader_connectome.R
@@ -2,7 +2,7 @@
 library(readxl)
 library(dplyr)
 
-path_master='/Users/ali/Desktop/Jul/apoe/MasterSheet_Experiments2021.xlsx'
+path_master='/Users/ali/Desktop/Jun23/apoe2_paper/divide_by_sum/multi_cca/MasterSheet_Experiments2021.xlsx'
 data=read_xlsx(path_master, sheet = '18ABB11_readable02.22.22_BJ_Cor' )
 datatemp=data%>%dplyr::select(DWI,Genotype,Weight, Sex, Diet, Age_Months,  CIVM_ID)#subselect
 #nchar(datatemp[111,1])
@@ -15,10 +15,10 @@ datatemp$DWI=as.numeric(substr(datatemp$DWI,2,6)) # make dwi numeric
 datatemp=datatemp[datatemp$Genotype=="APOE22",]
 
 ####
-path_connec="/Users/ali/Desktop/Jul/apoe/apoe234_connectomes/"
+path_connec="/Users/ali/Desktop/Jun23/apoe2_paper/divide_by_sum/connectome/"
 file_list=list.files(path_connec)
-temp_conn= read_xlsx( paste0(path_connec,file_list[1]) )
-temp_conn=temp_conn[,2: dim(temp_conn)[2]]
+temp_conn= read.csv( paste0(path_connec,file_list[1]) , header=F)
+#temp_conn=temp_conn[,2: dim(temp_conn)[2]]
 connectivity=array( NA ,dim=c(dim(temp_conn)[1],dim(temp_conn)[2],dim(datatemp)[1]))
 dim(connectivity)
 
@@ -29,9 +29,13 @@ for (i in 1:dim(connectivity)[3]) {
   temp_index=which(datatemp$DWI[i]==as.numeric(substr(file_list,2,6)))
   if (length(temp_index)>0) 
   {
-  temp_connec=read_xlsx( paste0(path_connec,file_list[temp_index]) )
-  temp_connec=temp_connec[,2:dim(temp_connec)[2]]
+  temp_connec=read.csv( paste0(path_connec,file_list[temp_index]) , header = F)
+  #temp_connec=temp_connec[,2:dim(temp_connec)[2]]
   colnames(temp_connec)=NA
+  temp_connec=as.matrix(temp_connec) ##
+  diag(temp_connec)=0### diag is 0
+  #temp_connec=temp_connec/sum(temp_connec) ### divide by sum of connectviity
+  #temp_connec=100*temp_connec
   connectivity[,,i]=as.matrix(temp_connec)
   }
   else
@@ -42,15 +46,15 @@ for (i in 1:dim(connectivity)[3]) {
 # notfound=notfound[2:length(notfound)]
 # not_found_index=which( datatemp$DWI  %in%  notfound )
 
-datatemp=datatemp[-not_found_index,]
-connectivity=connectivity[,,-not_found_index]
+#datatemp=datatemp[-not_found_index,]
+#connectivity=connectivity[,,-not_found_index]
 sum(is.na(connectivity))
 
 response=datatemp
 #setwd(system("pwd", intern = T) )
 
 
-RNA=read.delim('/Users/ali/Desktop/may/rnarpackage/micewithsymbol.txt',sep="\t", header = T)
+RNA=read.delim('/Users/ali/Desktop/Jun23/apoe2_paper/divide_by_sum/micewithsymbol.txt',sep="\t", header = T)
 RNA=t(RNA)
 RNA_rows=rownames(RNA)
 
